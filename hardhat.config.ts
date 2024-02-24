23dd7d09f32b38944bcf7d95e7aed16b0bbb5259
IjayAbby <inyangasi4@gmail.com>
2024-02-24 05:02:11 +0100

    Deploy on opensea marketplace

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,21 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+require("@nomicfoundation/hardhat-toolbox");
+require ("dotenv").config();
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+const { API_KEY,ALCHEMY_POLYGON_API_KEY_URL, ACCOUNT_PRIVATE_KEY, POLYGON_SCAN_API_KEY} = process.env;
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.19",
+  defaultNetwork: "mumbai",
+  networks: {
+    hardhat: {},
+    mumbai: {
+      url: ALCHEMY_POLYGON_API_KEY_URL,
+      accounts: [ `0x${ACCOUNT_PRIVATE_KEY}`],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.POLYGON_SCAN_API_KEY,
+  },
+  
 };
-
-export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,27 +1,17 @@
 import { ethers } from "hardhat";
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+const main = async () => {
+  try {
+    const nftContractFactory = await ethers.getContractFactory("onChainNFT");
+    const nftContract = await nftContractFactory.deploy();
+    await nftContract.waitForDeployment();
 
-  const lockedAmount = ethers.parseEther("0.001");
+    console.log("Contract deployed to:", nftContract.target);
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
-}
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main();
